b243d8ac261b153e1bb9deee568f5302a8bbc59d
minghaoBD <liminghao03@baidu.com>
2022-06-27 11:45:29 +0000

    add a gpu variable to the scope

--- paddle/fluid/inference/tensorrt/plugin/fc_plugin.cu
+++ paddle/fluid/inference/tensorrt/plugin/fc_plugin.cu
@@ -21,44 +21,52 @@ namespace plugin {
 
 FcPluginDynamic::FcPluginDynamic(std::string weights_name, std::string bias_name, framework::Scope* scope, int dev_id)
     : weights_name_(weights_name), bias_name_(bias_name), scope_(scope), dev_id_(dev_id), k_(0), n_(0) {
-        VLOG(1) << "[DEBUG] FcPluginDynamic";
-        VLOG(1) << "[DEBUG] buildtime scope addr " << scope;
-        VLOG(1) << "[debug] scope has kid " << scope_->kids().size();
+        std::string gpu_suffix = "_gpu_for_fc";
+        std::string weights_name_gpu = weights_name + gpu_suffix;
+        auto* Y_v_gpu = scope_->FindVar(weights_name_gpu);
+       
+        if (Y_v_gpu == nullptr) {
+            VLOG(1) << "[DEBUG] FcPluginDynamic";
+            VLOG(1) << "[DEBUG] buildtime scope addr " << scope;
+            VLOG(1) << "[debug] scope has kid " << scope_->kids().size();
 
-        auto* Y_v = scope_->FindVar(weights_name_); // auto == Variable
-        auto* Y_t = Y_v->GetMutable<framework::LoDTensor>();
-        auto dims = Y_t->dims();
-        k_ = dims[0];
-        VLOG(1) << "[DEBUG] k_ " << k_;
-        n_ = dims[1];
-        VLOG(1) << "[DEBUG] n_ " << n_;
+            auto* Y_v = scope_->FindVar(weights_name_); // auto == Variable
+            auto* Y_t = Y_v->GetMutable<framework::LoDTensor>();
+            auto dims = Y_t->dims();
+            k_ = dims[0];
+            VLOG(1) << "[DEBUG] k_ " << k_;
+            n_ = dims[1];
+            VLOG(1) << "[DEBUG] n_ " << n_;
 
-        VLOG(1) << "weights tensor type: " << Y_t->name();
-        VLOG(1) << "weights tensor element num: " << Y_t->numel();
-        VLOG(1) << "weights tensor dims" << Y_t->dims();
+            VLOG(1) << "weights tensor type: " << Y_t->name();
+            VLOG(1) << "weights tensor element num: " << Y_t->numel();
+            VLOG(1) << "weights tensor dims" << Y_t->dims();
 
-        VLOG(1) << "weights tensor dtype" << Y_t->dtype();
-        VLOG(1) << "weights tensor layout"<< Y_t->layout();
+            VLOG(1) << "weights tensor dtype" << Y_t->dtype();
+            VLOG(1) << "weights tensor layout"<< Y_t->layout();
 
-        //quant weights
-        float* old_weight_data = Y_t->data<float>();
-        framework::LoDTensor temp_tensor;
+            //quant weights
+            float* old_weight_data = Y_t->data<float>();
+            framework::LoDTensor temp_tensor;
 
-        //erase old weights var
-        scope_->EraseVars({weights_name_}); 
-
-        //create new weights var
-        framework::Variable *new_weight_v = scope_->Var(weights_name_);
-        framework::LoDTensor* new_weight_t = new_weight_v -> GetMutable<framework::LoDTensor>();
-        new_weight_t -> Resize(dims);
-        int8_t* new_weight_data = new_weight_t->mutable_data<int8_t>(platform::CUDAPlace(dev_id_));
-        std::vector<int8_t> new_weight_data_h(new_weight_t->numel(), 12);
-        VLOG(1) << "copy " << new_weight_t->numel() * sizeof(int8_t) << "bytes to device";
-        cudaMemcpy(new_weight_data, new_weight_data_h.data(), new_weight_t->numel() * sizeof(int8_t), cudaMemcpyHostToDevice);
-
-        VLOG(1) << "new weights tensor type: " << new_weight_t->name();
-        VLOG(1) << "new weights tensor element num: " << new_weight_t->numel();
-        VLOG(1) << "new weights tensor dims" << new_weight_t->dims();
+            //create new weights var
+            framework::Variable *Y_v_gpu = scope_->Var(weights_name_gpu);
+            framework::LoDTensor* new_weight_t = Y_v_gpu -> GetMutable<framework::LoDTensor>();
+            new_weight_t -> Resize(dims);
+            int8_t* new_weight_data = new_weight_t->mutable_data<int8_t>(platform::CUDAPlace(dev_id_));
+            std::vector<int8_t> new_weight_data_h(new_weight_t->numel(), 12);
+            VLOG(1) << "copy " << new_weight_t->numel() * sizeof(int8_t) << "bytes to device";
+            cudaMemcpy(new_weight_data, new_weight_data_h.data(), new_weight_t->numel() * sizeof(int8_t), cudaMemcpyHostToDevice);
+            VLOG(1) << "new weights tensor type: " << new_weight_t->name();
+            VLOG(1) << "new weights tensor element num: " << new_weight_t->numel();
+            VLOG(1) << "new weights tensor dims" << new_weight_t->dims();
+        } else {
+            VLOG(1) << "gpu weight is initialized from scope";
+            auto* Y_t_gpu = Y_v_gpu->GetMutable<framework::LoDTensor>();
+            VLOG(1) << "copied weights tensor type: " << Y_t_gpu->name();
+            VLOG(1) << "copied weights tensor element num: " << Y_t_gpu->numel();
+            VLOG(1) << "copied weights tensor dims" << Y_t_gpu->dims();  
+        }
 }
 
 
